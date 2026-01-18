26224d3ec08d3119afe88647eb1ca6ca9c46daf3
vladimirbutyrskii <vlaver@mail.ru>
2026-01-18 20:07:17 +0300

    first commit

---
+++ catalog/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.shortcuts import render
+
+
+def home(request):
+    return render(request, 'home.html')
+
+
+def contacts(request):
+    return render(request, 'contacts.html')
